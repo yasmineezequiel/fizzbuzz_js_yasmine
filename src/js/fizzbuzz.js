2deee985ef75a539c07e514e6dc1d6b4e27f44a7
yasmine <yasmine.ezequiel@gmail.com>
2019-09-14 18:23:35 +0200

    Adds FizzBuzz method into the code

--- src/js/fizzbuzz.js
+++ src/js/fizzbuzz.js
@@ -1,12 +1,16 @@
-function FizzBuzz() {
+function FizzBuzz()  {
   this.check = (number) => {
-    if (number % 3 === 0) {
+    if (number % 15 === 0) {
+      return 'FizzBuzz';
+    } else if (number % 5 === 0) {
+        return 'Buzz';
+    } else if (number % 3 === 0) {
       return 'Fizz';
     } else {
       return number
     }
   }
-}
+};
 
   if (typeof module !== 'undefined' && module.exports) {
     module.exports = FizzBuzz;
