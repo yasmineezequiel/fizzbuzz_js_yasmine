2b15c94ab749043b23973b4d0dcb0f317f8e3079
yasmine <yasmine.ezequiel@gmail.com>
2019-09-14 18:54:53 +0200

    Adds button feature into HTML

--- features/application.feature.js
+++ features/application.feature.js
@@ -1,20 +1,19 @@
 require('../spec.helper');
 
-context('Your Description of the test scenario', () => {
-  // Initialize a browser and visit the server's root path
+  describe('User can input a value and get FizzBuzz results', () => {
   before(async () => {
     await browser.init()
     await browser.visitPage('http://localhost:8080/')
   });
 
-  // Reload before each test 
+
   beforeEach(async () => {
     await browser.page.reload();
   });
 
   // Make sure the browser closes after the test is finished
-  after(() => {
-    browser.close();
+  after(async () => {
+    await browser.close();
   });
 
   // Example test
